bad4a2e64e56788ec46bcfca4c81fe0a05541175
c0a251964f <c0a251964f@edu.teu.ac.jp>
2026-06-16 15:48:39 +0900

    演習１，２

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -2,6 +2,7 @@ import os
 import sys
 import pygame as pg
 import random
+import time
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -20,6 +21,39 @@ def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     if rct.top < 0 or HEIGHT < rct.bottom:
         tate = False
     return yoko, tate
+
+
+def gameover(screen: pg.Surface) -> None:
+    black = pg.Surface((WIDTH, HEIGHT))
+    pg.draw.rect(black, (0, 0, 0), pg.Rect(0, 0, WIDTH, HEIGHT))
+    black.set_alpha(180)
+    fonto = pg.font.Font(None, 80)
+    txt = fonto.render("Game Over", True, (255, 255, 255))
+    txt_rct = txt.get_rect()
+    txt_rct.center = WIDTH // 2, HEIGHT // 2
+    black.blit(txt, txt_rct)
+    kk_img_l = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    kk_img_r = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9) 
+    kk_rct_l = kk_img_l.get_rect()
+    kk_rct_r = kk_img_r.get_rect()
+    kk_rct_l.center = WIDTH // 2 - 200, HEIGHT // 2
+    kk_rct_r.center = WIDTH // 2 + 200, HEIGHT // 2
+    black.blit(kk_img_l, kk_rct_l)
+    black.blit(kk_img_r, kk_rct_r)
+    screen.blit(black, [0, 0])
+    pg.display.update()
+    time.sleep(5)
+
+
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    bb_imgs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)
+        bb_img.set_colorkey((0, 0, 0))
+        bb_imgs.append(bb_img)
+    bb_accs = [a for a in range(1, 11)]
+    return bb_imgs, bb_accs
 
 
 def main():
@@ -48,12 +82,14 @@ def main():
         pg.K_LEFT:(-5, 0), 
         pg.K_RIGHT:(+5, 0),
     }
+    bb_imgs, bb_accs = init_bb_imgs()
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
         if kk_rct.colliderect(bb_rct):
             print("ゲームオーバー")
+            gameover(screen)
             return
         screen.blit(bg_img, [0, 0]) 
 
@@ -68,7 +104,12 @@ def main():
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1]) #動きをキャンセル
         screen.blit(kk_img, kk_rct)
 
-        bb_rct.move_ip([vx, vy])
+        avx = vx*bb_accs[min(tmr//500, 9)]
+        avy = vy*bb_accs[min(tmr//500, 9)]
+        bb_img = bb_imgs[min(tmr//500, 9)]
+        bb_rct.width = bb_img.get_rect().width
+        bb_rct.height = bb_img.get_rect().height
+        bb_rct.move_ip([avx, avy])
         yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
